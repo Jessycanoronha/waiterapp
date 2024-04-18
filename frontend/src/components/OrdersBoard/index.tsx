ef96445d6fd64b82e9d69aa17b90cf733186e081
Jessyca Noronha <jessycarozeene@gmail.com>
2024-04-17 21:15:23 -0300

    feat: Add toast logic

--- frontend/src/components/OrdersBoard/index.tsx
+++ frontend/src/components/OrdersBoard/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Order } from '../../types/Order';
 import { OrderModal } from '../OrderModal';
+import { toast, Bounce, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Board, OrdersContainer } from './styles';
 
 type OrdersBoardProps = {
@@ -8,37 +10,100 @@ type OrdersBoardProps = {
   title: string;
   orders: Order[];
 }
+
 export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [toastDisplayed, setToastDisplayed] = useState(false);
+  const [toastErrorDisplayed, setToastErrorDisplayed] = useState(false);
+
+  const handleClose = (startProduction: boolean) => {
+    setIsOpenModal(false);
+    setSelectedOrder(null);
+    if (startProduction && !toastDisplayed) {
+      setToastDisplayed(true);
+    }
+  };
+
+  useEffect(() => {
+    if (toastDisplayed) {
+      notify();
+    }
+    if (toastErrorDisplayed) {
+      notifyCancel();
+    }
+  }, [toastDisplayed, toastErrorDisplayed]);
+
+  const notify = () => {
+    toast.success('Pedido iniciado com sucesso!', {
+      position: 'top-right',
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      progress: undefined,
+      theme: 'light',
+      transition: Bounce,
+      toastId: 'success',
+    });
+  };
+  const notifyCancel = () => {
+    toast.error('Pedido cancelado!', {
+      position: 'top-right',
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      progress: undefined,
+      theme: 'light',
+      transition: Bounce,
+      toastId: 'error',
+    });
+  };
 
   function handleOpenOrderDetails(order: Order) {
-    setIsOpenModal(true);
+    setToastDisplayed(false);
+    setToastErrorDisplayed(false);
     setSelectedOrder(order);
+    setIsOpenModal(true);
+  }
+  function handleCancelOrder(canceled: boolean) {
+    setIsOpenModal(false);
+    setSelectedOrder(null);
+    if (!toastDisplayed && canceled) {
+      setToastErrorDisplayed(true);
+    }
   }
 
   return (
-    <Board>
-      {isOpenModal &&
-        <OrderModal visible={isOpenModal} order={selectedOrder} onClose={() => setIsOpenModal(false)} />
-      }
-      <header>
-        <span>{icon}</span>
-        <strong>{title}</strong>
-        <span>({orders?.length})</span>
-      </header>
+    <>
+      <ToastContainer />
+      <Board>
+        {isOpenModal && (
+          <OrderModal
+            visible={isOpenModal}
+            order={selectedOrder}
+            onClose={() => setIsOpenModal(false)}
+            handleClose={handleClose}
+            handleCancel={handleCancelOrder}
+          />
+        )}
 
-      {orders.length > 0 && (
-        <OrdersContainer>
-          {orders.map((order) => (
-            <button type="button" key={order.id} onClick={() => handleOpenOrderDetails(order)}>
-              <strong>{order.table_number}</strong>
-              <span>{order.products.length} itens</span>
-            </button>
-          ))}
-        </OrdersContainer>
-      )}
+        <header>
+          <span>{icon}</span>
+          <strong>{title}</strong>
+          <span>({orders?.length})</span>
+        </header>
 
-    </Board>
+        {orders.length > 0 && (
+          <OrdersContainer>
+            {orders.map((order) => (
+              <button type="button" key={order.id} onClick={() => handleOpenOrderDetails(order)}>
+                <strong>{order.table_number}</strong>
+                <span>{order.products.length} itens</span>
+              </button>
+            ))}
+          </OrdersContainer>
+        )}
+      </Board>
+    </>
   );
 }
