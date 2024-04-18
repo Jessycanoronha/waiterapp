a9551c629b642628865d485e7097c8ca9ae4277c
Jessyca Noronha <jessycarozeene@gmail.com>
2024-04-18 04:45:51 -0300

    refactor: Move to utils

---
+++ frontend/src/utils/returnCategoryName.ts
@@ -0,0 +1,11 @@
+export function returnCategoryName(id: number | string) {
+  if (id == 1) {
+    return '🍕 Pizza';
+  }
+  if (id == 2) {
+    return '🍔 Hambúrguer';
+  }
+  if (id == 3) {
+    return '🥤 Bebidas';
+  }
+}
