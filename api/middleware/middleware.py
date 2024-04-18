4d019494d73d974b2c1927d239dc5d7825abdfe3
Jessyca Noronha <jessycarozeene@gmail.com>
2024-04-18 04:45:13 -0300

    fix: Add PATH to middleware

--- api/middleware/middleware.py
+++ api/middleware/middleware.py
@@ -8,7 +8,7 @@ def configure_cors(app):
     def add_cors_headers(response):
         response.headers['Access-Control-Allow-Origin'] = '*' 
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
         return response
 
 def create_upload_folder(app):
