027f48fd864e70faff2052450a4375e6554312f0
Jessyca <jessycarozeene@gmail.com>
2024-04-19 00:59:49 -0300

    feat: Use dotenv instead directly envs

--- api/app.py
+++ api/app.py
@@ -1,15 +1,17 @@
-# api/app.py
-
 from flask import Flask
 from routes import category_routes, product_routes, order_routes
 from models import db
 import middleware
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/waiterappflask'
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -20,9 +22,8 @@ category_routes.initialize_routes(app)
 product_routes.initialize_routes(app)
 order_routes.initialize_routes(app)
 
-
-SWAGGER_URL="/swagger"
-API_URL="/static/swagger.json"
+SWAGGER_URL = "/swagger"
+API_URL = "/static/swagger.json"
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
@@ -32,8 +33,6 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
     }
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000, debug=True)
