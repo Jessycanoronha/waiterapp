5c2faafdd7520e624cc0e3b319ebd1e1d80e3341
Jessyca Noronha <jessycarozeene@gmail.com>
2024-04-18 21:23:48 -0300

    refactor: Change swagger app name

--- api/app.py
+++ api/app.py
@@ -28,7 +28,7 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': 'Access API'
+        'app_name': 'WaiterApp'
     }
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
